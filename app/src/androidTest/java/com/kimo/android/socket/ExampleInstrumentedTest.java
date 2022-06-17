0895eedf4894299fbc60c52a5732bf4b91997c1f
Kimo Android <69405523+kimoandroid@users.noreply.github.com>
2022-06-17 17:04:12 +0200

    Add files via upload

---
+++ app/src/androidTest/java/com/kimo/android/socket/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.kimo.android.socket;
+
+import static junit.framework.Assert.assertEquals;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.kimo.android.socket", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/kimo/android/socket/Constants.java
@@ -0,0 +1,5 @@
+package com.kimo.android.socket;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "https://encept.co";
+}
